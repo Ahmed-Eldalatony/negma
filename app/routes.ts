7723457db73cbf122e3340aeb0d7dea8c78d367a
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-10-20 19:16:51 +0300

    cleanup

--- app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("category/:id", "routes/category.$id.tsx"),
-  route("product/:id", "routes/product.$id.tsx"),
-] satisfies RouteConfig;
