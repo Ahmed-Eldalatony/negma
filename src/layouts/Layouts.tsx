fc4ccc1ba3991bef3bf1d94afb5dab93eba1d391
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-10-26 22:48:33 +0300

    responsive ui fixes

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: any) {
 	return (
-		<div style={{ direction: 'rtl' }} className="w-[450px] px-2">
+		<div style={{ direction: 'rtl' }} className="w-[320px] sm:w-[450px]">
 			{children}
 		</div>
 	);
