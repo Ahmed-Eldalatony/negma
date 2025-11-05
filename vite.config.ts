ecc51c08892e834851bf4403161cbca67e576892
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-11-06 01:36:59 +0200

    test fixing 404 on reload

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,20 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://boddasaad.me',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	server: {
+		historyApiFallback: true,
+		proxy: {
+			'/api': {
+				target: 'https://boddasaad.me',
+				changeOrigin: true,
+				secure: true,
+			},
+		},
+	},
 });
